638e8990a34844ff8eff538fe69c2f25087d3037
Haichhan <ppfreea1@gmail.com>
2025-01-26 22:43:38 +0700

    fix migrations

---
+++ Migrations/20250126154311_update3.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace anime_comics.Migrations
+{
+    /// <inheritdoc />
+    public partial class update3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_pages_books_BookId",
+                table: "pages");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "pages",
+                newName: "ChapterId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_pages_BookId",
+                table: "pages",
+                newName: "IX_pages_ChapterId");
+
+            migrationBuilder.CreateTable(
+                name: "volumes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BookId = table.Column<long>(type: "bigint", nullable: false),
+                    VolumeNo = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    CoverImg = table.Column<string>(type: "text", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_volumes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_volumes_books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chapters",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    VolumeId = table.Column<long>(type: "bigint", nullable: false),
+                    ChapNo = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    PageCount = table.Column<int>(type: "integer", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chapters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chapters_volumes_VolumeId",
+                        column: x => x.VolumeId,
+                        principalTable: "volumes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chapters_VolumeId",
+                table: "Chapters",
+                column: "VolumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_volumes_BookId",
+                table: "volumes",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pages_Chapters_ChapterId",
+                table: "pages",
+                column: "ChapterId",
+                principalTable: "Chapters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_pages_Chapters_ChapterId",
+                table: "pages");
+
+            migrationBuilder.DropTable(
+                name: "Chapters");
+
+            migrationBuilder.DropTable(
+                name: "volumes");
+
+            migrationBuilder.RenameColumn(
+                name: "ChapterId",
+                table: "pages",
+                newName: "BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_pages_ChapterId",
+                table: "pages",
+                newName: "IX_pages_BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pages_books_BookId",
+                table: "pages",
+                column: "BookId",
+                principalTable: "books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
